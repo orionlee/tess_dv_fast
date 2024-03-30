ab3a8f6aaef63646bd55c23e5276e941d3895329
orionlee <orionlee@yahoo.com>
2024-03-29 17:06:36 -0700

    Make Google Cloud Run container smaller - use "slim" base image, container size down from 266M to 120M

--- tess_dv_fast_webapp_gcloud/assemble.sh
+++ tess_dv_fast_webapp_gcloud/assemble.sh
@@ -14,7 +14,8 @@ mkdir -p $dest/data/tess_dv_fast
 # --update --archive
 cp --update --archive  $base/../data/tess_dv_fast/tess_tcestats.db  $dest/data/tess_dv_fast
 
-cp --update --archive  $base/*.*  $dest
+cp --update --archive  $base/*  $dest
+cp --update --archive  $base/.*  $dest
 cp --update --archive  $base/../tess_dv_fast.py $base/../tess_dv_fast_webapp.py  $dest
 
 echo SQLite database included in the deployment:
