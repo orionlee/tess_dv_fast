6de4e2ab46fcaac99f3928e59e0b7463d37fa3f9
orionlee <orionlee@yahoo.com>
2025-12-24 18:55:27 -0800

    webapp: fixed js error seen in devtools console when there is no SPOC TCE

--- tess_dv_fast_webapp.py
+++ tess_dv_fast_webapp.py
@@ -289,8 +289,8 @@ def tces():
 
         <script src="https://cdn.jsdelivr.net/gh/javve/list.js@2.3.1/dist/list.min.js"></script>
         <script>
-            if (document.querySelector('#result table')) {{
-                // init search/sort only if there is a result table
+			// Init in-table sort/filter with list.js for SPOC results (if exists)
+            if (document.querySelector('#result table#table_spoc')) {{
                 const options = {{valueNames: ['col0', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 'col10', 'col11']}};
                 const tceList = new List('result', options);
             }}
