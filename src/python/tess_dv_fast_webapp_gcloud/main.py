cffaa68df53c58a14f0d7ddcfd87bdfcc40bf04f
orionlee <orionlee@yahoo.com>
2026-07-07 14:13:12 -0700

    Remove the extra gcloud main.py from legacy location

--- src/python/tess_dv_fast_webapp_gcloud/main.py
+++ src/python/tess_dv_fast_webapp_gcloud/main.py
@@ -6,9 +6,9 @@ from tess_dv_fast import tess_dv_fast_webapp
 # Entrypoint for Google Cloud Run deployment
 #
 app = tess_dv_fast_webapp.app  # Google Cloud Run requires `app` attribute in main
-if __name__ == "__main__":
+if __name__ == "__main__":  # for local testing only. not used by GCR
     app.run(
-        debug=True,
+        debug=False,
         use_reloader=False,  # disable hot reload and avoid the extra watcher process
         use_evalex=False,  # disable werkzeug's web-based  interactive debugger on error page
         host=os.environ.get("TESS_TCES_HOST", "0.0.0.0"),
